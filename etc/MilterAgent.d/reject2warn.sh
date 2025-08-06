7676bb3d60abd2998c5d9230663652b4b19c4f3d
T.Kabu <kabu@fvg-on.net>
2025-08-06 12:35:24 +0900

    Add REJECT to WARN change shell script for Linux

--- etc/MilterAgent.d/reject2warn.sh
+++ etc/MilterAgent.d/reject2warn.sh
@@ -2,4 +2,4 @@
 
 TARGET_DIR="./"  # ← ここを対象ディレクトリに変更
 
-find "$TARGET_DIR" -type f -exec sed -i '' 's/:REJECT/:WARN/g' {} +
+find "$TARGET_DIR" -type f -name "*.conf" -exec sed -i 's/:REJECT/:WARN/g' {} +
